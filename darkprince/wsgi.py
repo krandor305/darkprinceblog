6c8cf6fe85862518377f2e3da2063fb3606598fc
erris <al-had@hotmail.com>
2019-05-09 17:42:32 +0200

    3

--- darkprince/wsgi.py
+++ darkprince/wsgi.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
+settings_module = "settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
 from django.core.wsgi import get_wsgi_application
-from mezzanine.utils.conf import real_project_name
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "%s.settings" % real_project_name("darkprince"))
-
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
