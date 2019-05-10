46e13d97a5ac6d6dd2a9e53363c206406f318db7
erris <al-had@hotmail.com>
2019-05-10 02:51:00 +0200

    3

--- darkprince/settings.py
+++ darkprince/settings.py
@@ -176,16 +176,14 @@ CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_APP
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = "https://babablog.herokuapp.com/darkprince/static"#"/static/"
+STATIC_URL = "/static/"
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = "darkprince/static"
-STATICFILES_DIRS = [
-    "darkprince/static",
-]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
